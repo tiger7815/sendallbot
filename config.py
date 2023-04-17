c981bbfcac0927dbd8b296d9ffbc5e953d0dc61d
PratikLPP <119912597+PratikLPP@users.noreply.github.com>
2023-04-17 21:02:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6150468285:AAGIG6MiFTHpeOpUf7YZWssgODqH3rlc_2k"
+    BOT_TOKEN = "6057569026:AAHCtAYygH9rV-gISWH_eY3dE0zcnftEJJE"
     API_ID = 10577960
     API_HASH = "80fd047285f4e94ca80311928b6bb5da"
     AUTH_USERS = '5593532344'
